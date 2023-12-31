952ce3fd7100da098b33b373683dd885ef07ebcc
Edgars Skrabiņš <shiftkiro@gmail.com>
2023-12-31 14:10:51 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+let timeDifference = ref<string>();
+const refreshIntervalInMs = 100;
+
+const countTimeDifference = () => {
+  const currentDate = new Date(Date.now());
+  const targetDate = new Date(2024, 0, 1);
+
+  const difference = targetDate.getTime() / 1000 - currentDate.getTime() / 1000;
+  timeDifference.value = difference.toFixed(0);
+}
+
+setInterval(countTimeDifference, refreshIntervalInMs)
+
+</script>
+
+<template>
+  <div>
+    <h1>{{timeDifference}}</h1>
+  </div>
+</template>
+
+<style scoped>
+  h1{
+    font-size: 25rem;
+    font-weight:bolder;
+    filter: drop-shadow(0 0 0.6rem rgba(0, 0, 0, 0.45));
+  }
+</style>
